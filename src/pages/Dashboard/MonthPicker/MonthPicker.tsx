1556b7d7fd847f950e558a6c1f65a65a4ad993ef
Artyom Ivanov <Artyom655@yandex.ru>
2023-06-09 01:43:17 +0300

    добавь react-datepicker

--- src/pages/Dashboard/MonthPicker/MonthPicker.tsx
+++ src/pages/Dashboard/MonthPicker/MonthPicker.tsx
@@ -1,17 +1,26 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import ru from "date-fns/locale/ru";
+registerLocale("ru", ru);
 
 import styles from "./MonthPicker.module.css";
 
-const date = new Date();
-const curYear = date.getFullYear().toString();
-let curMonth = date.getMonth().toString();
-if (+curMonth < 10) curMonth = "0" + curMonth;
-
 const MonthPicker: FC = () => {
+  const [monthYear, setMonthYear] = useState(new Date());
+
   return (
     <div className={styles["month-picker"]}>
       <label htmlFor="month-picker">Месяц</label>
-      <input id="month-picker" min="1970-01" type="month" defaultValue={`${curYear}-${curMonth}`} />
+      <DatePicker
+        selected={monthYear}
+        onChange={(date) => setMonthYear(date ? date : new Date())}
+        minDate={new Date("1970")}
+        maxDate={new Date()}
+        dateFormat="MMMM yyyy"
+        showMonthYearPicker
+        locale="ru"
+      />
     </div>
   );
 };
