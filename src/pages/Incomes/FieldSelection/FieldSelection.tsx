9d58aecebc578848cf8167250299d0eeda8ebd2f
Artyom Ivanov <Artyom655@yandex.ru>
2023-06-19 23:23:50 +0300

    убери overflow-x для main компонента

--- src/pages/Incomes/FieldSelection/FieldSelection.tsx
+++ src/pages/Incomes/FieldSelection/FieldSelection.tsx
@@ -1,11 +1,11 @@
-import { FC} from "react";
+import { FC } from "react";
 import styles from "./FieldSelection.module.css";
 import Field from "../../../components/Income/Field/Field";
 import FieldMonate from "../../../components/Income/Field/FieldMonate/FieldMonate";
 import SelectorBox from "../../../components/SelectorBox/SelectorBox";
-interface SelectionProps{
-  monthYear:Date;
-  setMonthYear:React.Dispatch<React.SetStateAction<Date>>;
+interface SelectionProps {
+  monthYear: Date;
+  setMonthYear: React.Dispatch<React.SetStateAction<Date>>;
   sumBoxRefs: React.MutableRefObject<HTMLInputElement>;
   CommentBoxRefs: React.MutableRefObject<HTMLTextAreaElement>;
   CategoryBoxRef: React.MutableRefObject<HTMLSelectElement>;
@@ -16,20 +16,27 @@ const selectorBoxOptions = [
   { text: "Подарок", value: "Подарок" },
   { text: "Подработка", value: "Подработка" },
 ];
-const addNewPost =()=>{
-   
-}
-const FieldSelection: FC<SelectionProps> = ({sumBoxRefs,CommentBoxRefs,CategoryBoxRef,monthYear,setMonthYear})=>{
+const addNewPost = () => {};
+const FieldSelection: FC<SelectionProps> = ({
+  sumBoxRefs,
+  CommentBoxRefs,
+  CategoryBoxRef,
+  monthYear,
+  setMonthYear,
+}) => {
   return (
-
     <div className={styles.fieldselection}>
-    <Field value="sum" reff={sumBoxRefs}/>
-   <FieldMonate monthYear={monthYear} setMonthYear={setMonthYear} />
-    <form className={styles["chart-form"]}>
-      <SelectorBox options={selectorBoxOptions} label="Категория" CategoryBoxRef={CategoryBoxRef}/>
-    </form>
-    <textarea placeholder="Введите описание"  ref={CommentBoxRefs}></textarea>
- </div>
-  ); 
+      <Field value="sum" reff={sumBoxRefs} />
+      <FieldMonate monthYear={monthYear} setMonthYear={setMonthYear} />
+      <form className={styles["chart-form"]}>
+        <SelectorBox
+          options={selectorBoxOptions}
+          label="Категория"
+          CategoryBoxRef={CategoryBoxRef}
+        />
+      </form>
+      <textarea placeholder="Введите описание" ref={CommentBoxRefs}></textarea>
+    </div>
+  );
 };
-export default FieldSelection
+export default FieldSelection;
