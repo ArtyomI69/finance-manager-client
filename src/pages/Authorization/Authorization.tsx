482c3fe78c1edb27ce5002ca878e49affd78ddeb
Artyom Ivanov <Artyom655@yandex.ru>
2023-07-03 21:03:33 +0300

    добавь ConfirmEmail компонент

--- src/pages/Authorization/Authorization.tsx
+++ src/pages/Authorization/Authorization.tsx
@@ -3,12 +3,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import styles from "./Authorization.module.css";
 import LoginSignUp from "./LoginSignUp/LoginSignUp";
+import ConfirmEmail from "./ConfirmEmail/ConfirmEmail";
 
 const Authorization: FC = () => {
   return (
     <div className={styles.authorization}>
       <Routes>
         <Route path="/authorization/*" element={<LoginSignUp />} />
+        <Route path="/confirmEmail" element={<ConfirmEmail />} />
         <Route path="/*" element={<Navigate to="/authorization" />} />
       </Routes>
     </div>
