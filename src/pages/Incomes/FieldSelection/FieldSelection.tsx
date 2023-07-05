61362c524f8cd91b5d705b6ca335f145077e3322
Artyom Ivanov <Artyom655@yandex.ru>
2023-07-05 12:06:00 +0300

    сделай так чтобы поле описание было не обязательным

--- src/pages/Incomes/FieldSelection/FieldSelection.tsx
+++ src/pages/Incomes/FieldSelection/FieldSelection.tsx
@@ -1,4 +1,4 @@
-import { FC, useState,useEffect } from "react";
+import { FC, useState, useEffect } from "react";
 import { Formik, Form, FormikHelpers } from "formik";
 import { string, object, ref, ObjectSchema, number } from "yup";
 import styles from "./FieldSelection.module.css";
@@ -11,28 +11,28 @@ import { userAPI } from "../../../store/services/UserService";
 import { ICategories } from "../../../models/ICategories";
 import { toast } from "react-toastify";
 import { IIncomes } from "../../../models/IIncomes";
+import { transactionsAPI } from "../../../store/services/TransactionsService";
 
-interface FieldSelectionProps{
+interface FieldSelectionProps {
   monthYear: Date;
   setMonthYear: React.Dispatch<React.SetStateAction<Date>>;
   onSubmit: (values: IIncomes, onSubmitProps: FormikHelpers<IIncomes>) => void;
 }
 
+const FieldSelection: FC<FieldSelectionProps> = ({ monthYear, setMonthYear, onSubmit }) => {
+  const { data, isLoading, isError } = transactionsAPI.useFetchCategoryQuery();
+  const [selectorBoxOptions, setSelectorBoxOptions] = useState<ICategories[]>([]);
 
-const FieldSelection: FC<FieldSelectionProps> = ({monthYear,setMonthYear,onSubmit }) => {
-const { data, isLoading, isError } = userAPI.useFetchCategoryQuery();
-const [selectorBoxOptions, setSelectorBoxOptions] = useState<ICategories[]>([]);
-
-useEffect(() => {
-  if (isError) toast.error("Не удалось загрузить данные! Пожалуйста повторите попытку позже");
-  if (data) {
-    const options: ICategories[] = data.map((category: ICategories) => ({
-      id: category.id,
-      name: category.name,
-    }));
-    setSelectorBoxOptions(options);
-  }
-}, [data, isError]);
+  useEffect(() => {
+    if (isError) toast.error("Не удалось загрузить данные! Пожалуйста повторите попытку позже");
+    if (data) {
+      const options: ICategories[] = data.map((category: ICategories) => ({
+        id: category.id,
+        name: category.name,
+      }));
+      setSelectorBoxOptions(options);
+    }
+  }, [data, isError]);
   const initialValues = {
     amount: null,
     createdAt: 0,
@@ -40,33 +40,34 @@ useEffect(() => {
     description: "",
   };
   const validationSchema = object({
-    amount: number().required("Необходимо заполнить данное поле").positive('Число должно быть положительным').typeError('Должно быть числом'),
+    amount: number()
+      .required("Необходимо заполнить данное поле")
+      .positive("Число должно быть положительным")
+      .typeError("Должно быть числом"),
     createdAt: number().required("Необходимо заполнить данное поле"),
-    description: string().required("Необходимо заполнить данное поле"),
+    description: string(),
   });
   return (
     <>
       <div className={styles.fieldselection}>
-        
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={onSubmit}
-            >
-              {(formik) => (
-                <Form>
-                  <div>
-                    <FieldInput type="text" name="amount" placeholder={'Введите сумму'}  />
-                    <FieldMonate monthYear={monthYear} setMonthYear={setMonthYear} />
-                    <SelecterField options={selectorBoxOptions} name="category" formik={formik} />
-                    <FieldInput type="text" name="description" placeholder={'Введите комментарий'} />
-                  </div>
-                  <ButtonAdd formik={formik} value="Сохранить"/>
-                </Form>
-              )}
-            </Formik>
-          </div>
-
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {(formik) => (
+            <Form>
+              <div>
+                <FieldInput type="text" name="amount" placeholder={"Введите сумму"} />
+                <FieldMonate monthYear={monthYear} setMonthYear={setMonthYear} />
+                <SelecterField options={selectorBoxOptions} name="category" formik={formik} />
+                <FieldInput type="text" name="description" placeholder={"Введите комментарий"} />
+              </div>
+              <ButtonAdd formik={formik} value="Сохранить" />
+            </Form>
+          )}
+        </Formik>
+      </div>
     </>
   );
 };
--- src/pages/MoneyTransfers/InputSelection/InputSelection.tsx
+++ src/pages/MoneyTransfers/InputSelection/InputSelection.tsx
@@ -1,55 +1,58 @@
 import { FC, useState } from "react";
 import { Formik, Form, FormikHelpers } from "formik";
-import { string, object, ref, ObjectSchema,number } from "yup";
+import { string, object, ref, ObjectSchema, number } from "yup";
 import styles from "./InputSelection.module.css";
 import FieldInput from "../../../components/Income/Field/FieldInput";
 import FieldMonate from "../../../components/Income/Field/FieldMonate/FieldMonate";
 import ButtonAdd from "../../../components/Income/Field/ButtonAdd/ButtonAdd";
 import { ISelectionInput } from "./ISelectionInput";
 import { ITransfers } from "../../../models/ITransfers";
-interface FieldSelectionProps{
+interface FieldSelectionProps {
   monthYear: Date;
   setMonthYear: React.Dispatch<React.SetStateAction<Date>>;
   onSubmit: (values: ITransfers, onSubmitProps: FormikHelpers<ITransfers>) => void;
 }
-const FieldSelection: FC<FieldSelectionProps> = ({monthYear,setMonthYear,onSubmit }) => {
- 
-
+const FieldSelection: FC<FieldSelectionProps> = ({ monthYear, setMonthYear, onSubmit }) => {
   const initialValues: ITransfers = {
-    person_to_id:null,
+    person_to_id: null,
     amount: null,
     createdAt: monthYear.getTime(),
-    description:""
+    description: "",
   };
 
-  const validationSchema: ObjectSchema<ITransfers> = object({
-    person_to_id: number().required("Необходимо заполнить данное поле").positive('Число должно быть положительным').typeError('Должно быть числом'),
-    amount: number().required("Необходимо заполнить данное поле").positive('Число должно быть положительным').typeError('Должно быть числом'),
+  const validationSchema = object({
+    person_to_id: number()
+      .required("Необходимо заполнить данное поле")
+      .positive("Число должно быть положительным")
+      .typeError("Должно быть числом"),
+    amount: number()
+      .required("Необходимо заполнить данное поле")
+      .positive("Число должно быть положительным")
+      .typeError("Должно быть числом"),
     createdAt: number().required("Необходимо заполнить данное поле"),
-    description:string().required("Необходимо заполнить данное поле"),
+    description: string(),
   });
   return (
     <>
       <div className={styles.fieldselection}>
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={onSubmit}
-            >
-              {(formik) => (
-                <Form>
-                  <div>
-                  <FieldInput type="text" name="person_to_id" placeholder={'Введите id'} />
-                    <FieldInput type="text" name="amount" placeholder={'Введите сумму'} />
-                    <FieldMonate monthYear={monthYear} setMonthYear={setMonthYear} />
-                    <FieldInput type="text" name="description" placeholder={'Введите комментарий'} />
-                  </div>
-                  <ButtonAdd formik={formik} value="Сохранить"/>
-                </Form>
-              )}
-            </Formik>
-          </div>
-
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {(formik) => (
+            <Form>
+              <div>
+                <FieldInput type="text" name="person_to_id" placeholder={"Введите id"} />
+                <FieldInput type="text" name="amount" placeholder={"Введите сумму"} />
+                <FieldMonate monthYear={monthYear} setMonthYear={setMonthYear} />
+                <FieldInput type="text" name="description" placeholder={"Введите комментарий"} />
+              </div>
+              <ButtonAdd formik={formik} value="Сохранить" />
+            </Form>
+          )}
+        </Formik>
+      </div>
     </>
   );
 };
